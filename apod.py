14c966be8ea5e2ce2a5f48abf3b9a7d0780b787a
Will Soula <william.soula@starz.com>
2021-07-13 14:03:41 -0600

    Guard against videos

--- apod.py
+++ apod.py
@@ -26,10 +26,13 @@ class Apod(BotPlugin):
         url = 'http://apodapi.herokuapp.com/api/?date='+year+'-'+month+'-'+day
         page = urllib.request.Request(url)
         response = json.loads(urllib.request.urlopen(page).read().decode('utf-8'))
-        image_url = response['url']
-        image_bytes = requests.get(image_url).content
-        file = open('tmp.gif', 'wb')
-        file.write(image_bytes)
-        file.close()
-        self.send_stream_request(msg.frm, open('tmp.gif', 'rb'), name='image.gif', stream_type='image/gif')
-        self.send(response['description'])
+        if 'url' in response:
+            image_url = response['url']
+            image_bytes = requests.get(image_url).content
+            file = open('tmp.gif', 'wb')
+            file.write(image_bytes)
+            file.close()
+            self.send_stream_request(msg.frm, open('tmp.gif', 'rb'), name='image.gif', stream_type='image/gif')
+            self.send(response['description'])
+        else:
+            self.send('No image for:'+year+'-'+month+'-'+day)
