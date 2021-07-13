9f6e2a954862e1b2cd2a8d2204ab6029ed1c3e71
Will Soula <william.soula@starz.com>
2021-07-13 14:01:47 -0600

    Default to today, allow random with -r flag, send description too

--- apod.py
+++ apod.py
@@ -4,17 +4,24 @@ import json
 import secrets
 from errbot import BotPlugin, botcmd, arg_botcmd, re_botcmd
 import requests
+from datetime import date
 
 
 class Apod(BotPlugin):
     """Astronomy Picture Of the Day"""
 
-    @botcmd
-    def apod(self, msg, args):
+    @arg_botcmd('-r', dest='random', type=bool, default=False)
+    def apod(self, msg, random=False):
         """Astronomy Picture Of the Day"""
-        year = str(secrets.choice(range(2000, 2021)))
-        month = str(secrets.choice(range(1, 13)))
-        day = str(secrets.choice(range(1, 29)))
+        current_date = date.today()
+        if random:
+            year = str(secrets.choice(range(2000, current_date.year)))
+            month = str(secrets.choice(range(1, 13)))
+            day = str(secrets.choice(range(1, 29)))
+        else:
+            year = current_date.year
+            month = current_date.month
+            day = current_date.day
         # apodapi.herokuapp.com/api/?date=2001-07-12
         url = 'http://apodapi.herokuapp.com/api/?date='+year+'-'+month+'-'+day
         page = urllib.request.Request(url)
@@ -25,3 +32,4 @@ class Apod(BotPlugin):
         file.write(image_bytes)
         file.close()
         self.send_stream_request(msg.frm, open('tmp.gif', 'rb'), name='image.gif', stream_type='image/gif')
+        self.send(response['description'])
